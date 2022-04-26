de9324edd44261e7e3c0732cd81d58a7431e0495
Prch1016 <Christianpross95@googlemail.com>
2022-04-26 16:10:02 +0200

    Update main.py
    Kick Off

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import cv2
+import time
 
-print("Hello World!")
+cam = cv2.VideoCapture(1)
+
+
+
+# Capture frame-by-frame    
+ret, frame = cam.read() 
+
+cv2.imshow('frame', frame)
+
+
+
+while (True):
+    #Waits for a user input to quit the application    
+    if cv2.waitKey(1) & 0xFF == ord('q'):    
+        break
+
+#cam.release()
+#cv2.destroyAllWindows()
